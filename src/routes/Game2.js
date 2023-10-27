f7a0574f340d78f5f9de87b6c29db423d99e6597
kimyurie <9911dbfl@naver.com>
2023-10-27 16:38:29 +0900

    Feat: 셀 클릭 처리 및 돌 색상 변경 구현

--- src/routes/Game2.js
+++ src/routes/Game2.js
@@ -3,8 +3,9 @@ import { useState, useEffect } from "react";
 // 오목
 function Game2() {
   var row = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  var [count, setCount] = useState(60);
   var [start, setStart] = useState(true);
+  var [color, setColor] = useState(''); // 돌의 색상
+  var [clickedCell, setClickedCell] = useState([]); // 클릭한 셀의 위치
 
   useEffect(() => {
     let a = setTimeout(() => {
@@ -15,6 +16,17 @@ function Game2() {
     };
   }, []);
 
+  const handleCellClick = (i,j) => {
+    // 클릭한 셀의 위치가 배열에 추가되도록
+    setClickedCell([...clickedCell, {i,j}]);
+    // 현재 클릭된 셀의 돌의 색 저장
+    const newColor = color === 'black' ? 'white' : 'black';
+    // 현재 클릭된 셀의 돌 색 변경
+    document.getElementById(`cell-${i}-${j}`).style.backgroundColor = newColor;
+    // 현재 클릭된 돌의 색 변경
+    setColor(newColor);
+  }
+
   return (
     <div>
       {start == true ? <p>* 흑돌 먼저 시작 !</p> : <p>&nbsp;</p>}
@@ -22,11 +34,16 @@ function Game2() {
       <table className="tb2" onClick={() => {
       }}>
         <tbody>
-          {row.map(function () {
+          {row.map(function (i) {
             return (
               <tr>
-                {row.map(function (a, i) {
-                  return <td onClick={() => console.log(i)}></td>;
+                {row.map(function (j) {
+                  return <td 
+                    id = {`cell-${i}-${j}`}
+                    className = {clickedCell.some(a => a.i === i & a.j === j) ? color : ''} 
+                    onClick={() => 
+                    handleCellClick(i,j) 
+                  }></td>;
                 })}
               </tr>
             );
