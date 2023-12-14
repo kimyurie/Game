412096ea5d59f87829dfc0f7c91dc13c520bc6dc
kimyurie <9911dbfl@naver.com>
2023-12-14 17:53:40 +0900

    post

--- src/routes/Game1.js
+++ src/routes/Game1.js
@@ -1,29 +1,61 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 // 지뢰찾기
 function Game1() {
-  var col = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  var [count, setCount] = useState(60);
+  let [col,setCol] = useState(Array.from({length:13},()=>Array.from({length:13},()=>-1)))
+  let [count, setCount] = useState(60);
   let [color, setColor] = useState('');
-  var [clickedCell, setClickedCell] = useState([]); // 클릭한 칸의 좌표
-
+  let [clickedCell, setClickedCell] = useState(new Set());
+  // console.log(col)
   const handleCellClick = (i, j) => {
-    setClickedCell([...clickedCell, {i,j}])
-    setColor('white_line')
+    const newColor = color === '' ? 'white_line' : color;
+    setColor(newColor);
+
+    // 클릭한 셀의 좌표를 Set에 추가
+    const newClickedCell = new Set(clickedCell);
+    newClickedCell.add(`${i}-${j}`);
+    setClickedCell(newClickedCell);
+
+    // 눌려있는데 숫자 없으면 0, 안눌려있으면 -1(지뢰 상관없이)
+    const encodeGameBoard = () => {
+          console.log()
+          return col
+            .map((a,i)=>{
+              return a.map((b,j)=>{
+                return newClickedCell.has(`${i}-${j}`) ? '0' : '-1'
+              }).join("")
+            }).join("/")
+        }
+      
+
+    axios
+    .post("https://jsonplaceholder.typicode.com/posts", {
+      // location: (13 - i) + "," + (j-1),
+      location: (i) + "," + (j),
+      situation : encodeGameBoard(),
+    })
+    .then((res) => {
+      setCol(res.data.situation.split("/").reverse().map(e=>e.split("")))
+      console.log(res.data);
+    })
+    .catch((err) => {
+      console.log("실패");
+    }); 
   }
 
-  useEffect(() => {
-    let a = setInterval(function () {
-      count -= 1;
-      if (count >= 0) {
-        setCount(count);
-      }
-    }, 1000);
-
-    return () => {
-      clearTimeout(a);
-    };
-  }, []);
+  // useEffect(() => {
+  //   let a = setInterval(function () {
+  //     count -= 1;
+  //     if (count >= 0) {
+  //       setCount(count);
+  //     }
+  //   }, 1000);
+  
+  //   return () => {
+  //     clearTimeout(a);
+  //   };
+  // }, []);
 
   return (
     <div>
@@ -31,18 +63,22 @@ function Game1() {
 
       <table className="tb1">
         <tbody>
-          {col.map((i) => {
+        {col.map((_,i) => {
             return (
               <tr>
-                {col.map((j) => {
+                {col.map((_,j) => {
                   return (
                     <td
                       // 클릭한 칸이 흑 -> 백으로 바뀌게
-                      className={clickedCell.some((e) => e.i === i && e.j === j) ? color : ''}
-                      onClick={() => {
-                        handleCellClick(i, j)
-                      }}
-                    ></td>
+                      id={`cell-${i}-${j}`}
+                      className={clickedCell.has(`${12-i}-${j}`)? color : ''}
+                      onClick={() => {handleCellClick(12-i, j)}} 
+                    >
+                  {/* 흰색 칸 안에 숫자 1 표시  */}
+                  {clickedCell.has(`${12-i}-${j}`) && color === 'white_line' && (
+                    <div className="number">1</div>
+                  )}
+                    </td>
                   );
                 })}
               </tr>
@@ -55,3 +91,4 @@ function Game1() {
 }
 
 export default Game1;
+
